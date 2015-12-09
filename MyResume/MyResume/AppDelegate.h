eee075ec9f2c8e9c3eccaa900e7b9ddc11313f49
BigPi <wangdapishuai@163.com>
2015-12-09 18:15:02 +0800

    initial and add HandWritting Animation

---
+++ MyResume/MyResume/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyResume
+//
+//  Created by pi on 15/12/9.
+//  Copyright © 2015年 pi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
